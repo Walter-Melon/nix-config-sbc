b2f7d8259ea033cfad101865b9938ad27d57b220
Walter-Melon <mc.walter.melon@gmail.com>
2025-06-09 10:45:28 +0000

    Added VSCode hack

--- flake.nix
+++ flake.nix
@@ -4,21 +4,35 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+
+    # Needed to connect via ssh over VSCode
+    vscode-server.url = "github:nix-community/nixos-vscode-server";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs = { self, nixpkgs, vscode-server, ... }@inputs:
     let
       inherit (self) outputs;
     in
     {
       nixosConfigurations = {
+        # Image for a headless SD-Card image
         pi4-installer = nixpkgs.lib.nixosSystem {
           specialArgs = { inherit inputs outputs; };
           modules = [ ./hosts/pi4-installer ];
         };
+
+        # Configuration for the TV
         pi4-tv = nixpkgs.lib.nixosSystem {
           specialArgs = { inherit inputs outputs; };
-          modules = [ ./hosts/pi4-tv ];
+          modules = [
+            ./hosts/pi4-tv
+
+            # VSCode server hack
+            vscode-server.nixosModules.default
+            ({ config, pkgs, ... }: {
+              services.vscode-server.enable = true;
+            })
+          ];
         };
       };
     };
