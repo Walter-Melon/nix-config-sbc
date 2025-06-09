b10d194950da517c0a17d426949bb4fcecabee8d
Walter-Melon <mc.walter.melon@gmail.com>
2025-06-09 14:17:29 +0000

    Added home-manager and hyprland

--- flake.nix
+++ flake.nix
@@ -5,11 +5,16 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    
+    hyprland.url = "github:hyprwm/Hyprland";
+
     # Needed to connect via ssh over VSCode
     vscode-server.url = "github:nix-community/nixos-vscode-server";
   };
 
-  outputs = { self, nixpkgs, vscode-server, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, hyprland, vscode-server, ... }@inputs:
     let
       inherit (self) outputs;
     in
@@ -24,8 +29,28 @@
         # Configuration for the TV
         pi4-tv = nixpkgs.lib.nixosSystem {
           specialArgs = { inherit inputs outputs; };
+          pkgs = nixpkgs.legacyPackages.aarch64-linux;
+
           modules = [
             ./hosts/pi4-tv
+            
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+
+              home-manager.extraSpecialArgs = inputs // specialArgs;
+              home-manager.users.tv = import ./users/tv/home.nix;
+            }
+
+            {
+              wayland.windowManager.hyprland = {
+                enable = true;
+                # set the flake package
+                package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+                portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+              };
+            }
 
             # VSCode server hack
             vscode-server.nixosModules.default
